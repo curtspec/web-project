4eca3d6b84c815e3492709cd19982c0f7042a5fa
henry chu <chuhd89@gmail.com>
2019-04-12 14:31:07 +0900

    final

--- asset/js/chu/menu_open.js
+++ asset/js/chu/menu_open.js
@@ -1,3 +1,41 @@
 $(document).ready(function(){
     $('.menu-top-visual').animate({"height":"600px","opacity":"1"},500);
+
+    $('.menu-item-hover').hide();
+
+    $('.menu-item-visual').on('mouseenter',function(){
+        $(this).children('.menu-item-hover').clearQueue();
+        $(this).children('.menu-item-hover').slideToggle(500);
+        $(this).closest('li').css("border","1px solid black");
+    });
+    $('.menu-item-visual').on('mouseleave',function(){
+        $(this).children('.menu-item-hover').clearQueue();
+        $(this).children('.menu-item-hover').slideToggle(500);
+        $(this).closest('li').css("border","1px solid gainsboro");
+    });
+    $('.allround_dropdown_title').on('mouseenter', function(){
+        $(this).animate({
+            opacity : 1
+        }, {queue:false, duration:400});
+    });
+    $('.allround_dropdown_title').on('mouseleave', function(){
+        $(this).animate({
+            opacity : 0.4
+        }, {queue:false, duration:400});
+    });
+
+    $('.allround_dropdown_contents').hide();
+
+    $('.allround_dropdown_title').on('click', function(){
+        var s =$(this).next();
+        $(s).slideToggle('slow');
+        $(s).clearQueue();
+        $(s).attr("tabindex", -1).focus();
+        $(s).on('blur',function(){
+            if( $(s).is(':visible') ){
+                $(s).slideUp('slow');
+                $(s).clearQueue();
+            }
+        });
+    });
 });
