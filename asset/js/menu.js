e727e933f31d35a3e43d85e93237096d95bd6e2d
Kim Hyeong Woo <khw5917@gmail.com>
2019-04-12 13:57:30 +0900

    Commit

--- asset/js/menu.js
+++ asset/js/menu.js
@@ -13,4 +13,29 @@ $(document).ready(function(){
         $(this).children('.menu-item-hover').slideToggle(500);
         $(this).closest('li').css("border","1px solid gainsboro");
     });
+    $('.allround_dropdown_title').on('mouseenter', function(){
+        $(this).animate({
+            opacity : 1
+        }, {queue:false, duration:400});
+    });
+    $('.allround_dropdown_title').on('mouseleave', function(){
+        $(this).animate({
+            opacity : 0.4
+        }, {queue:false, duration:400});
+    });
+
+    $('.allround_dropdown_contents').hide();
+
+    $('.allround_dropdown_title').on('click', function(){
+        var s =$(this).next();
+        $(s).slideToggle('slow');
+        $(s).clearQueue();
+        $(s).attr("tabindex", -1).focus();
+        $(s).on('blur',function(){
+            if( $(s).is(':visible') ){
+                $(s).slideUp('slow');
+                $(s).clearQueue();
+            }
+        });
+    });
 });
